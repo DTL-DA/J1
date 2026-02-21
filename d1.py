c26713609b01c4f093b0e3444818213a0f985578
DTL-DA <donnyst@uninorte.edu.co>
2026-02-20 19:23:39 -0500

    Create d1.py

---
+++ d1.py
@@ -0,0 +1,7 @@
+echo "# J1" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/DTL-DA/J1.git
+git push -u origin main
